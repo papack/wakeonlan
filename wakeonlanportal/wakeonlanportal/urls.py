cc457c32cc999c97a5d3a460bb0b0845950ab0b8
Matthias Steiner <m@kjpjm.de>
2020-02-29 13:57:08 +0100

    Add information MainPage

--- wakeonlanportal/wakeonlanportal/urls.py
+++ wakeonlanportal/wakeonlanportal/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
     path('systemlog/', include('systemlog.urls')),
+    path('information/', include('information.urls')),
 ]
