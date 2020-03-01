df5c4206b61ed2eeafc8b244c8cf04e69e9c3dfd
Matthias Steiner <m@kjpjm.de>
2020-03-01 14:43:54 +0100

    add edit and delete for devices + disallow normal user adding + deleting devices

--- wakeonlanportal/devices/urls.py
+++ wakeonlanportal/devices/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns =[
     path('', views.all_devices, name="all_devices"),
     path('new', views.new_device, name="new_device"),
+    path('<slug:name>/edit', views.edit_device, name="edit_device"),
+    path('<slug:name>/del', views.del_device, name="del_device"),
 ]
