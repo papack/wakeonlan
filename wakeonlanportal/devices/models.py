5cfe302d9cfd4a31857b8c61f96f8de0070d4405
Matthias Steiner <m@kjpjm.de>
2020-03-01 09:42:50 +0100

    not force the user to input a Description

--- wakeonlanportal/devices/models.py
+++ wakeonlanportal/devices/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Device(models.Model):
     name = models.SlugField()
     mac = models.CharField(max_length=10)
-    desc = models.CharField(max_length=200)
+    desc = models.CharField(max_length=200, null=True, blank=True)
 
     def __str__(self):
         return self.name+"("+self.mac+")"
