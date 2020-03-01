c68b5ea307f4706786976558caeaf3dd9727501d
Matthias Steiner <m@kjpjm.de>
2020-03-01 16:30:48 +0100

    Delete Syslog. this feature will be added in a later version

--- wakeonlanportal/systemlog/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SystemlogConfig(AppConfig):
-    name = 'systemlog'
--- wakeonlanportal/systemlog/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def systemlog(request):
-    return render(request, 'systemlog/log.html')
