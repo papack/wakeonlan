3c4a8f13aacc44cfb8a992c4db6ddabfffb43866
Matthias Steiner <m@kjpjm.de>
2020-03-01 09:17:16 +0100

    create Model for Devices

--- wakeonlanportal/devices/models.py
+++ wakeonlanportal/devices/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Device(models.Model):
+    name = models.SlugField()
+    mac = models.CharField(max_length=10)
+    desc = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name+"("+self.mac+")"
