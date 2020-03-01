83297678c83ab163641966dec5b17d32a9053cff
Matthias Steiner <m@kjpjm.de>
2020-03-01 14:41:26 +0100

    change login and logout redirections

--- wakeonlanportal/wakeonlanportal/settings.py
+++ wakeonlanportal/wakeonlanportal/settings.py
@@ -123,3 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#Default login Page
+LOGIN_URL = '/account/login'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = LOGIN_URL
