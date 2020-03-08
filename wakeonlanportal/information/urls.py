f5b734a771b88e73f8d0e4c53c18bd62cf6f95b7
Matthias Steiner <m@kjpjm.de>
2020-03-08 10:53:24 +0100

    remove old files, prepare for the first version

--- wakeonlanportal/information/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns =[
-    path('help', views.help, name="help"),
-    path('about', views.about, name="about"),
-]
--- wakeonlanportal/information/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, HttpResponse
-
-# Create your views here.
-
-def about(request):
-    return render(request, 'information/about.html')
-
-def help(request):
-    return render(request, 'information/help.html')
